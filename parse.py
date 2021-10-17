00850d0285a1ae660cb7a32ca5e366c912a7c694
Barosl Lee <git@barosl.com>
2021-10-17 17:37:35 +0900

    Parse Java VM type signatures from original data

--- parse.py
+++ parse.py
@@ -5,6 +5,36 @@ from pathlib import Path
 import re
 import json
 
+PRIM_TYPES = {'Z': 'boolean', 'B': 'byte', 'C': 'char', 'S': 'short', 'I': 'int', 'J': 'long', 'F': 'float', 'D': 'double', 'V': 'void'}
+
+def parse_java_vm_sig_inner(sig):
+    tag = sig[0]
+    if prim_type := PRIM_TYPES.get(tag):
+        return prim_type, sig[1:]
+    elif tag == 'L':
+        pos = sig.index(';')
+        cls = sig[1:pos].replace('/', '.')
+        return cls, sig[pos+1:]
+    elif tag == '[':
+        ty, remaining = parse_java_vm_sig_inner(sig[1:])
+        return ty + '[]', remaining
+    elif tag == '(':
+        pos = sig.index(')')
+        arg_sig = sig[1:pos]
+        arg_tys = []
+        while arg_sig:
+            ty, arg_sig = parse_java_vm_sig_inner(arg_sig)
+            arg_tys.append(ty)
+        ret_ty, remaining = parse_java_vm_sig_inner(sig[pos+1:])
+        return f"{ret_ty}({', '.join(arg_tys)})", remaining
+    else:
+        raise RuntimeError(f"Invalid Java VM type signature: {sig}")
+
+def parse_java_vm_sig(sig):
+    parsed, remaining = parse_java_vm_sig_inner(sig)
+    assert not remaining
+    return parsed
+
 def proc(lines):
     exc_cls_sig = None
     exc_to_string_text = None
@@ -14,7 +44,7 @@ def proc(lines):
     for line in lines:
         key, val = line.split('=', 1)
         if key == 'exc_cls_sig':
-            exc_cls_sig = val
+            exc_cls_sig = parse_java_vm_sig(val)
         elif key == 'exc_to_string_text':
             exc_to_string_text = val
         elif key == 'frame_idx':
@@ -24,11 +54,11 @@ def proc(lines):
         elif key == 'method_name':
             cur_frame['method_name'] = val
         elif key == 'method_sig':
-            cur_frame['method_sig'] = val
+            cur_frame['method_sig'] = parse_java_vm_sig(val)
         elif key == 'line_num':
             cur_frame['line_num'] = int(val)
         elif key == 'method_cls_sig':
-            cur_frame['method_cls_sig'] = val
+            cur_frame['method_cls_sig'] = parse_java_vm_sig(val)
         elif key == 'method_cls_gen':
             cur_frame['method_cls_gen'] = val
         elif key == 'local_idx':
@@ -42,7 +72,7 @@ def proc(lines):
         elif key == 'local_val':
             cur_locals['val'] = val
         elif key == 'local_cls_sig':
-            cur_locals['cls_sig'] = val
+            cur_locals['cls_sig'] = parse_java_vm_sig(val)
 
     return {
         'exc_cls_sig': exc_cls_sig,
