909f506d7c25625ef4739d5781632ed9190366ad
Barosl Lee <git@barosl.com>
2021-10-17 22:35:31 +0900

    Emphasize only the last word of a type signature

--- web.py
+++ web.py
@@ -5,10 +5,21 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 import json
+from markupsafe import Markup
 
 app = FastAPI()
 db = None
 tpls = Jinja2Templates('tpls')
+
+def emp_last_word(text):
+    try:
+        pos = text.rindex('.') + 1
+    except ValueError:
+        return text
+    else:
+        return text[:pos] + Markup('<b>') + text[pos:] + Markup('</b>')
+
+tpls.env.filters['emp_last_word'] = emp_last_word
 
 @app.get('/', response_class=HTMLResponse)
 def index(req: Request):
