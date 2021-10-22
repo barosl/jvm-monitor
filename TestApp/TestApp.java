a1ccd62ee0879c6f8c368f511ef63d6bba1b0487
Barosl Lee <git@barosl.com>
2021-10-22 16:49:32 +0900

    Update TestApp

--- TestApp/TestApp.java
+++ TestApp/TestApp.java
@@ -6,33 +6,42 @@ class Inspect {
 }
 
 public class TestApp {
-    static void b() {
+    static void b(String text) {
         int a = 123;
         long b = 456;
         float c = 99.99f;
         double d = 7.5;
-        float e = 3.3f;
+        String[] words = new String[] {"akemi", "homura"};
         System.out.println("b()");
-        throw new RuntimeException("!!");
+        throw new RuntimeException("This is an exception");
     }
 
     static void a(int a_arg) {
         int a_local = 111;
         try {
-            b();
+            b("This is an argument");
         } finally {
             System.out.println("finally");
         }
     }
 
+    static void testTheAnswer() {
+        String hello = "world";
+        int answerToTheUniverse = 43;
+
+        if (answerToTheUniverse != 42) {
+            Inspect.inspect();
+        }
+    }
+
     public static void main(String[] args) {
-        double pi = 3.14;
-        System.out.println("This is a TestApp");
+        testTheAnswer();
+
+        System.out.println("This is TestApp");
         try {
             a(222);
         } catch (RuntimeException e2) {
             System.out.println("caught");
         }
-        Inspect.inspect();
     }
 }
